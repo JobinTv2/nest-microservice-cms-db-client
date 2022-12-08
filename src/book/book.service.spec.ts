8247b08dde2966902a921ee6b87907fbb06d5ef7
Jobin T v <jobintv@focaloid.com>
2022-12-08 19:18:37 +0530

    feat: Book service unit test

--- src/book/book.service.spec.ts
+++ src/book/book.service.spec.ts
@@ -5,7 +5,34 @@ import { Book } from './entities/book.entity';
 
 describe('BookService', () => {
   let service: BookService;
-  const mockBookRepository = {};
+  const books = [
+    {
+      id: Date.now(),
+      name: 'Book 2',
+      title: 'Book 2',
+      description: 'Description',
+      rating: 2,
+      is_sold: false,
+      owner_id: 1,
+      categroy: 'Romantic',
+      author: 'Author 2',
+      price: 130,
+      reviews: 'reviews',
+      createdAt: Date.now(),
+    },
+  ];
+  const mockBookRepository = {
+    save: jest.fn((dto) => {
+      return {
+        id: Date.now(),
+        ...dto,
+        createdAt: Date.now(),
+      };
+    }),
+    find: jest.fn(() => {
+      return books;
+    }),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -22,5 +49,54 @@ describe('BookService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should add a book', async () => {
+    expect(
+      await service.create({
+        name: 'Book 2',
+        title: 'Book 2',
+        description: 'Description',
+        rating: 2,
+        is_sold: false,
+        owner_id: 1,
+        categroy: 'Romantic',
+        author: 'Author 2',
+        price: 130,
+        reviews: 'reviews',
+      }),
+    ).toEqual({
+      id: expect.any(Number),
+      name: 'Book 2',
+      title: 'Book 2',
+      description: 'Description',
+      rating: 2,
+      is_sold: false,
+      owner_id: 1,
+      categroy: 'Romantic',
+      author: 'Author 2',
+      price: 130,
+      reviews: 'reviews',
+      createdAt: expect.any(Number),
+    });
+  });
+
+  it('should return all books', async () => {
+    expect(await service.findAll()).toEqual([
+      {
+        id: expect.any(Number),
+        name: 'Book 2',
+        title: 'Book 2',
+        description: 'Description',
+        rating: 2,
+        is_sold: false,
+        owner_id: 1,
+        categroy: 'Romantic',
+        author: 'Author 2',
+        price: 130,
+        reviews: 'reviews',
+        createdAt: expect.any(Number),
+      },
+    ]);
   });
 });
